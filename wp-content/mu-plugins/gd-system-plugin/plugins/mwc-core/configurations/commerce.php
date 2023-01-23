ae027293754a4b775c0d5c77b202e82a6821473c
site43180186 <site43180186@a2nlwpsftp005.prod.iad2.secureserver.net>
2023-01-23 21:29:16 +0000

    fix some gitignore issues

--- wp-content/mu-plugins/gd-system-plugin/plugins/mwc-core/configurations/commerce.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'customers' => [
-        'api' => [
-            'url' => defined('MWC_COMMERCE_CUSTOMERS_SERVICE_URL') ? MWC_COMMERCE_CUSTOMERS_SERVICE_URL : 'https://api.godaddy.com',
-        ],
-    ],
-];
--- wp-content/mu-plugins/gd-system-plugin/plugins/mwc-core/src/Features/Commerce/Customers/Providers/DataObjects/Contracts/DataObjectContract.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace GoDaddy\WordPress\MWC\Core\Features\Commerce\Customers\Providers\DataObjects\Contracts;
-
-use GoDaddy\WordPress\MWC\Common\Contracts\CanConvertToArrayContract;
-
-interface DataObjectContract extends CanConvertToArrayContract
-{
-}
