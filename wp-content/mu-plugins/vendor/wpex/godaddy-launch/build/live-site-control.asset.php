85029e147717e793ce21f6315836af80659f1875
site43180186 <site43180186@a2nlwpsftp011.prod.iad2.secureserver.net>
2023-05-17 17:46:01 +0000

    live site updates

---
+++ wp-content/mu-plugins/vendor/wpex/godaddy-launch/build/live-site-control.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => 'b1dcdaa6d616b7db25c6');
